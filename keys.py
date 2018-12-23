5902cf608742a61fc4e6907bd2b06406451fda9d
SameepYadav <Yadavsameep58@gmail.com>
2018-12-24 00:35:53 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,4 @@
+consumer_key = "63owYJIcWCOw5antuUniVWHzR" #Api generated key
+consumer_secret = "ljtWMaOTEslHIVgzHXEXG54kBQd4xG1314W7FrPJbX5WdjqRMM"
+acess_key = "2408867136-uFqIGNk5JanAtbpWHbryfExhUfLwcJljclTOe8y" #Acess token
+acess_secret = "nTpeVBLZU4yLiHhqxUFXaR447aLKp4FayrJ7Nncn8NdsH" 
